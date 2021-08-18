0de9eaa7ddff07cbd6769f195de642582fe77e72
atm-greg <gregory.blemand@atm-consulting.fr>
2021-08-18 09:44:37 +0200

    add deleted files

---
+++ sql/data.sql
@@ -0,0 +1,12 @@
+/*INSERT INTO llx_c_skill_type (rowid, code, label, entity, active)
+VALUES (1, 'SKILL_TYPE_SOFT_SKILL', 'Savoir etre', 0, 1)
+     ,(2, 'SKILL_TYPE_EXPERTISE', 'Savoir faire', 0, 1)
+     ,(3, 'SKILL_TYPE_KNOWLEDGE', 'Savoir', 0, 1);
+
+
+INSERT INTO llx_c_skill_type (rowid, code, label, entity, active)
+VALUES (1, 'SKILL_TYPE_SOFT_SKILL', 'Savoir etre', 0, 1)
+,(2, 'SKILL_TYPE_EXPERTISE', 'Savoir faire', 0, 1)
+,(3, 'SKILL_TYPE_KNOWLEDGE', 'Savoir', 0, 1);
+
+*/
